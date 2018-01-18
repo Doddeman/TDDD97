134d94d45fd617dfee86937ed05f2753fcf7faf4
Doddeman <david.wajngot@gmail.com>
2018-01-18 13:48:23 +0100

    one page

--- client.js
+++ client.js
@@ -1,9 +1,13 @@
-displayView = function(){
+displayView = function(view){
  // the code required to display a view
+ document.getElementById("clientviewer").innerHTML = view;
 };
 window.onload = function(){
  //code that is executed as the page is loaded.
  //You shall put your own custom code here.
  //window.alert() is not allowed to be used in your implementation.
- window.alert("Hello TDDD97!");
+ 
+    welcomeview  = document.getElementById('welcomeview').innerHTML;
+    
+    displayView(welcomeview);
 };
