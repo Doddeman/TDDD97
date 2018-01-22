4308b563f8bc1f1301fb2ab8ae96b9ce20f0ed20
simme812 <simonmedin812@gmail.com>
2018-01-22 16:22:58 +0100

    Started with profileview: worked with Step 6 & 7

--- client.js
+++ client.js
@@ -7,12 +7,12 @@ window.onload = function(){
     profileview  = document.getElementById('profileview').innerHTML;
 	if(localStorage.getItem('token') !== null && localStorage.getItem('token') !== undefined){
 		displayView(profileview);
-		console.log("profile, token: " + localStorage.getItem('token')); 
+		console.log("profile, token: " + localStorage.getItem('token'));
 	}
 	else{
 	displayView(welcomeview);
-	console.log("welcome, token: " + localStorage.getItem('token')); 
-	}	
+	console.log("welcome, token: " + localStorage.getItem('token'));
+	}
 };
 
 
@@ -60,8 +60,65 @@ function signIn(){
 		}
 		serverstub.signUp(user);
 		console.log(serverstub.signUp(user).success);
-		
+
 		return true;
-		
+
 	}
+};
+
+/*FUNCTIONS FOR ACCOUNT PANEL*/
+function changePassword(){
+  var oldPassword = document.getElementById('oldPassword').value;
+  var newPassword = document.getElementById('newPassword').value;
+  var token = localStorage.getItem('token');
+  var changedStatus = serverstub.changePassword(token, oldPassword, newPassword);
+  if(newPassword.length < 6){
+    console.log("Too short");
+  }
+  else {
+    if(changedStatus.success == false){
+      console.log("Could not change password");
+    }
+    else {
+      console.log("Password changed")
+    }
+  }
+};
+
+function signOut(){
+  var token = localStorage.getItem('token');
+  serverstub.signOut(token);
+  localStorage.removeItem(token);
+  displayView(welcomeview);
+};
+
+/*FUNCTIONS FOR SHOWING A PANEL*/
+function showHomePanel(){
+  document.getElementById('homeTab').style.backgroundColor = 'mediumBlue';
+  document.getElementById('browseTab').style.backgroundColor = 'darkmagenta';
+  document.getElementById('accountTab').style.backgroundColor = 'darkmagenta';
+
+  document.getElementById('homePanel').style.display = 'block';
+  document.getElementById('browsePanel').style.display = 'none';
+  document.getElementById('accountPanel').style.display = 'none';
+};
+
+function showBrowsePanel(){
+  document.getElementById('homeTab').style.backgroundColor = 'darkmagenta';
+  document.getElementById('browseTab').style.backgroundColor = 'mediumblue';
+  document.getElementById('accountTab').style.backgroundColor = 'darkmagenta';
+
+  document.getElementById('homePanel').style.display = 'none';
+  document.getElementById('browsePanel').style.display = 'block';
+  document.getElementById('accountPanel').style.display = 'none';
+};
+
+function showAccountPanel(){
+  document.getElementById('homeTab').style.backgroundColor = 'darkmagenta';
+  document.getElementById('browseTab').style.backgroundColor = 'darkmagenta';
+  document.getElementById('accountTab').style.backgroundColor = 'mediumblue';
+
+  document.getElementById('homePanel').style.display = 'none';
+  document.getElementById('browsePanel').style.display = 'none';
+  document.getElementById('accountPanel').style.display = 'block';
 };
