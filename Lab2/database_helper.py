5fbcf81f0d20febbec08abcf464ab4031afe9cf2
David Wajngot <davwa195@parlomba3.ida.liu.se>
2018-02-08 14:43:43 +0100

    real start of lab2

--- Lab2/database_helper.py
+++ Lab2/database_helper.py
@@ -4,30 +4,76 @@ DATABASE = 'database.db'
 
 
 
-def init_db(app):
-	with app.app_context():
-		db = get_db()
-		with app.open_resource('schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
-
-def get_db():
+'''def get_db():
 	db = getattr(g, '_database', None)
 	if db is None:
 		db = g._database = sqlite3.connect(DATABASE)
-		connection = sqlite3.connect(DATABASE)
-		c = connection.cursor()
+	return db'''
+
+#connect = sqlite3.connect('database.db')
+#c = connect.cursor()
+
+
+def add_user(user):
+	token = 'NULL'
+	mail = user['email']
+	firstname = user['firstname']
+	familyname = user['familyname']
+	gender = user['gender']
+	city = user['city']
+	country = user['country']
+	password = user['password']
+
+	conn = sqlite3.connect(DATABASE)
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO users VALUES (?,?,?,?,?,?,?,?)",\
+	[token, mail, firstname, familyname, gender, city, country, password])
+	conn.commit()
+	conn.close()
+
+def find_user(user_email):
+	conn = sqlite3.connect(DATABASE)
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM users WHERE email = (?)", [user_email])
+	result = cursor.fetchall()
+	for r in result:
+	    print(r)
+
+	conn.commit()
+	conn.close()
+	'''token = 'NULL'
+	mail = user['email']
+	firstname = user['firstname']
+	familyname = user['familyname']
+	gender = user['gender']
+	city = user['city']
+	country = user['country']
+	password = user['password']'''
+
+def try_login(email, password):
+	conn = sqlite3.connect(DATABASE)
+	cursor = conn.cursor()
+	cursor.execute("SELECT password FROM users WHERE email = (?)", [email])
+	data = cursor.fetchone()
+	db_password = data[0]
+
+	if len(db_password) >= 6 and db_password == password:
+		token = "prutt"
+		cursor.execute("UPDATE users SET token = ? WHERE email = ?", [token, email])
+		conn.commit()
+		conn.close()
+		return True
+	else:
+		conn.commit()
+		conn.close()
+		return False
 
 
 
-		# Insert a row of data
-		c.execute("INSERT INTO users VALUES (NULL, 'test@test', 'David', 'Wajngot', 'male', 'linkan', 'Sweden', '123123')")
 
+# Save (commit) the changes
+#conn.commit()
 
-		# Save (commit) the changes
-		connection.commit()
-
-		# We can also close the connection if we are done with it.
-		# Just be sure any changes have been committed or they will be lost.
-		connection.close()
-	return db
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+#conn.close()
--- Lab2/server.py
+++ Lab2/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database_helper import *
 app = Flask(__name__)
 
@@ -6,6 +6,50 @@ app = Flask(__name__)
 #def hello():
 #    return "Hello World, jdafbgka"
 
-if __name__== "__main__":
-    init_db(app)
-    app.run(port = 8000, debug = True)
+#def init_db(app):
+#	with app.app_context():
+#		db = get_db()
+#		with app.open_resource('schema.sql', mode='r') as f:
+#			if db is None:
+#				db.cursor().executescript(f.read())
+#		db.commit()
+
+@app.route('/', methods=['POST'])
+def sign_up():
+	user_email = request.json['email']
+	#Glom ej check for om user redan finns!
+	first_name = request.json['firstname']
+	family_name = request.json['familyname']
+	gender = request.json['gender']
+	city = request.json['city']
+	country = request.json['country']
+	password = request.json['password']
+	if len(password) >= 6:
+		user = {'email': user_email, 'firstname': first_name,\
+		'familyname': family_name, 'gender': gender, 'city': city,\
+		'country': country, 'password': password}
+		for value in user.values():
+			#user.get('Age')
+			if len(value) < 1:
+				return jsonify({'success': False, 'message': 'Empty variable'})
+		add_user(user)
+		return jsonify({'success': True, 'message': 'User successfully created'})
+	else:
+		return jsonify({'success': False, 'message': 'Password too short'})
+
+@app.route('/signin', methods=['POST'])
+def sign_in():
+	user_email = request.json['email']
+	password = request.json['password']
+
+	if try_login(user_email, password):
+		#token = "hejhej"
+		return jsonify({'success': True, 'message': 'Logged in successfully'})
+	else:
+		return jsonify({'success': False, 'message': 'Wrong username or password'})
+
+
+
+
+#if __name__== "__main__":
+#    app.run(port = 8000, debug = True)
